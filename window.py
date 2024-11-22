2a746facad0f1efd2a4a73cc0bf408ab5f18fc39
MRGAMING201712 <doriansantiago0205@gmail.com>
2024-11-22 15:44:27 -0500

    moved the buttons around

--- window.py
+++ window.py
@@ -14,16 +14,16 @@ main_window.geometry("1800x800")
 
 #buttons
 load_file = Button(main_window, text="Load file") # add, command = load
-load_file.pack(padx = 0, pady = 50)
+load_file.pack(side = "left", padx = (700,50))
 
 
 combine_plots = Button(main_window, text="Combine plots") # add, command = combine
-combine_plots.pack(padx = 0, pady = 50)
+combine_plots.pack(side = "left")
 
 
 
 alternate_plots1 = Button(main_window, text="Alternate plots") # extra credit, add, command = alternate_plots, alternates between low, mid, high plots
-alternate_plots1.pack(padx = 0, pady = 50)
+alternate_plots1.pack(side = "left", padx = 50)
 
 
 #add button and additional visual output (our choice)
